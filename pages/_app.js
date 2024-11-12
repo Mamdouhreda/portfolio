217edb0029b273d961493daccc4efc16d2506595
mamdohreda <mamdohreda@gmail.com>
2024-11-12 22:57:18 +0000

    Implement new portfolio section and update dependencies and make it headless

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,21 @@
 import TokyoState from "@/src/Context";
 import PreLoader from "@/src/layout/PreLoader";
 import "@/styles/globals.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+// Create an Apollo Client instance
+const client = new ApolloClient({
+  uri: process.env.NEXT_PUBLIC_WORDPRESS_GRAPHQL_ENDPOINT,
+  cache: new InMemoryCache(),
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <TokyoState>
-      <PreLoader />
-      <Component {...pageProps} />
-    </TokyoState>
+    <ApolloProvider client={client}>
+      <TokyoState>
+        <PreLoader />
+        <Component {...pageProps} />
+      </TokyoState>
+    </ApolloProvider>
   );
 }
