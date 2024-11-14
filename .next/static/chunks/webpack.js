37b2baf7059ccd8d7eec7c90aa5e4cb6c0927cfe
mamdohreda <mamdohreda@gmail.com>
2024-11-14 21:32:23 +0000

    update the portfolio page to load quikly

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4ea26b2b54163ebf"; }
+/******/ 		__webpack_require__.h = function() { return "82df5c748ad29ced"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
