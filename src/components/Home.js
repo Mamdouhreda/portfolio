cdf675cbedaaa75dc91df1b41bdfd90f9b11721d
mamdohreda <mamdohreda@gmail.com>
2025-12-31 19:29:27 +0000

    update twiter and x

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,7 +18,7 @@ const socialIcon = [
   },
   {
     id: 4,
-    iconName: "icon-x-squared",
+    iconName: "x-icon",
     link: "https://x.com/mamelsheshtawy",
   },
 ];
@@ -53,11 +53,24 @@ const Home = () => {
                   {socialIcon.map((item) => (
                     <li className="mr-[8px] inline-block" key={item.id}>
                       <a
-                        className="text-black text-[20px] transition-all duration-300 hover:text-black"
+                        className="text-black text-[20px] transition-all duration-300 hover:text-black inline-flex items-center justify-center"
                         href={item.link}
                         target="_blank"
+                        rel="noopener noreferrer"
                       >
-                        <i className={item.iconName} />
+                        {item.iconName === "x-icon" ? (
+                          <svg
+                            width="20"
+                            height="20"
+                            viewBox="0 0 24 24"
+                            fill="currentColor"
+                            className="inline-block"
+                          >
+                            <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+                          </svg>
+                        ) : (
+                          <i className={item.iconName} />
+                        )}
                       </a>
                     </li>
                   ))}
